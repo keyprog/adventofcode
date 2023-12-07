0d13193f55188ca7d1770768882c406306be2bd4
Alexander Pokislyuk <keyprog@gmail.com>
2023-12-07 10:02:36 -0500

    day 7

--- 2023/day7/Program.cs
+++ 2023/day7/Program.cs
@@ -2,9 +2,6 @@
 
 Array.Sort(hands, new HandsComparer());
 
-foreach (var h in hands)
-    Console.WriteLine(h);
-
 int result = hands.Select((h, i) => (i + 1) * h.Bid).Sum();
 Console.WriteLine(result);
 
@@ -93,23 +90,22 @@ record Hand(string Cards, int Bid, HandType Type)
     public int GetCardStrength(int i) => GetCardStrength(Cards[i]);
 
     public static int GetCardStrength(char c)
+    => c switch
     {
-        return c switch
-        {
-            'A' => 14,
-            'K' => 13,
-            'Q' => 12,
-            'J' => 1,
-            'T' => 10,
-            '9' => 9,
-            '8' => 8,
-            '7' => 7,
-            '6' => 6,
-            '5' => 5,
-            '4' => 4,
-            '3' => 3,
-            '2' => 2,
-            _ => throw new NotSupportedException()
-        };
-    }
+        'A' => 14,
+        'K' => 13,
+        'Q' => 12,
+        'J' => 1,
+        'T' => 10,
+        '9' => 9,
+        '8' => 8,
+        '7' => 7,
+        '6' => 6,
+        '5' => 5,
+        '4' => 4,
+        '3' => 3,
+        '2' => 2,
+        _ => throw new NotSupportedException()
+    };
+
 }
