36e833edcc476444edda956daf7f4cfca60363f9
Alexander Pokislyuk <keyprog@gmail.com>
2023-12-12 23:52:31 -0500

    day 12

---
+++ 2023/day12/CommonExtensions.cs
@@ -0,0 +1,44 @@
+using static System.Linq.Enumerable;
+static class CollectionsExtensions
+{
+    public static void Print<T>(this IEnumerable<T> collection) => Console.WriteLine(string.Join(',', collection));
+}
+
+static class Collections2DExtensions
+{
+    public static IEnumerable<(int Row, int Col, T Value)> ForEach<T>(this T[][] collection)
+        => from r in Range(0, collection.Length) from c in Range(0, collection[0].Length) select (r, c, collection[r][c]);
+
+    public static void Print<T>(this T[][] collection)
+    {
+        for (int r = 0; r < collection.Length; ++r)
+        {
+            for (int c = 0; c < collection[0].Length; ++c)
+            {
+                Console.Write(collection[r][c]);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+    }
+    public static void Print<T>(this T[,] collection)
+    {
+        for (int r = 0; r < collection.GetLength(0); ++r)
+        {
+            for (int c = 0; c < collection.GetLength(1); ++c)
+            {
+                Console.Write(collection[r, c]);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+    }
+}
+partial class JaggedArray<T>(T[][] input)
+{
+    private T[][] data = input;
+    public int RowsCount { get; } = input.Length;
+    public int ColsCount { get; } = input[0].Length;
+
+    public void Print() => data.Print();
+}
---
+++ 2023/day12/Program.cs
@@ -0,0 +1,75 @@
+﻿using static System.Linq.Enumerable;
+
+Parser.Repeats = 5;
+decimal total = File.ReadLines("input.txt").Select(Parser.ParseLine).Sum(r => r.CountArrangements());
+Console.WriteLine("Total: " + total);
+
+static class Parser
+{
+    public static int Repeats {get;set;} = 1;
+    public static Record ParseLine(string line) => ParseLine(line.AsSpan());
+    public static Record ParseLine(ReadOnlySpan<char> line)
+    {
+        int sp = line.IndexOf(' ');
+        string template = line[0..sp].ToString();
+        template = string.Join('?', Repeat(template, Repeats));
+        string broken = line[(sp + 1)..].ToString();
+        broken = string.Join(',', Repeat(broken, Repeats));
+        return new Record(template, broken.Split(',').Select(int.Parse).ToArray());
+    }
+}
+
+class Record(string template, int[] broken)
+{
+    public string Template { get; } = template;
+    public int[] Broken { get; } = broken;
+
+    public long CountArrangements()
+    {
+        int[] operational = new int[Broken.Length];
+        Array.Fill(operational, 1, 1, operational.Length - 1);
+
+        int free = Template.Length - Broken.Sum() - operational.Sum();
+        if (free == 0) return 1;
+        return CountArrangements(operational, free, 0, 0, new());
+    }
+
+    private long CountArrangements(int[] operational, int free, int index, int start, Dictionary<(int, int), long> memo)
+    {
+        if (memo.TryGetValue((start,index), out long arrangements)) 
+            return arrangements;
+
+        if (index == operational.Length)        
+            return IsOperational(start, Template.Length) ? 1 : 0;       
+
+        for (int n = 0; n <= free; ++n)
+        {
+            operational[index] += n;
+            if (IsOperational(start, start + operational[index])
+                && IsBroken(start + operational[index], start + operational[index] + Broken[index]))
+            {
+                arrangements += CountArrangements(operational, free - n, index + 1, start + operational[index] + Broken[index], memo);
+            }
+            operational[index] -= n;
+        }
+        memo[(start,index)] = arrangements;
+        return arrangements;
+    }
+
+    private bool IsOperational(int start, int end)
+    => IsRangeOf(Template, start, end, '.');
+    
+    private bool IsBroken(int start, int end)
+    => IsRangeOf(Template, start, end, '#');
+    
+    private static bool IsRangeOf(string template, int start, int end, char target)
+    {
+        for (int j = start; j < end; j++)
+        {
+            char c = template[j];
+            if (c != '?' && c != target)
+                return false;
+        }
+        return true;
+    }
+}
