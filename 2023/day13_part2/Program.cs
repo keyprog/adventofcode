dae78fb5f49eaf6b17441fc2b3415470a6f44571
Alexander Pokislyuk <keyprog@gmail.com>
2023-12-14 11:47:32 -0500

    day 13, 14

---
+++ 2023/day13_part2/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Reflection.Emit;
+
+var patterns = Parser.Parse(File.ReadLines("input.txt"));
+
+int res = patterns.Select(p => p.GetSymmetry()).Sum();
+Console.WriteLine(res);
+
+class Parser
+{
+    public static IEnumerable<Pattern> Parse(IEnumerable<string> lines)
+    {
+        List<string> pattern = new();
+        foreach (var line in lines)
+        {
+            if (line.Length == 0)
+            {
+                yield return new Pattern(pattern.ToArray());
+                pattern.Clear();
+            }
+            else
+            {
+                pattern.Add(line);
+            }
+        }
+        yield return new Pattern(pattern.ToArray());
+    }
+}
+
+class Pattern(string[] Data)
+{
+    public int ColsCount { get; } = Data[0].Length;
+    public int RowsCount { get; } = Data.Length;
+
+    public int GetSymmetry()
+    {
+        int? vert = FindVertSymmetry();
+        if (vert.HasValue) return vert.Value;
+        int? horz = FindHorsSymmetry();
+        return horz!.Value * 100;
+    }
+
+    public int? FindVertSymmetry()
+    {
+        for (int c = 0; c < ColsCount - 1; ++c)
+        {
+            if(IsVertSymmetry(Data, c))
+                return c + 1;
+        }
+        return null;
+    }
+
+    private bool IsVertSymmetry(string[] Data, int c)
+    {
+        int smugdeCount = 0;
+        int checkWidth = Math.Min(c + 1, ColsCount - c - 1);
+        for (int cc = 0; cc < checkWidth; cc++)
+        {
+            for (int r = 0; r < RowsCount; ++r)
+            {
+                if (Data[r][c - cc] != Data[r][c + 1 + cc])
+                {
+                    smugdeCount++;
+                    if(smugdeCount > 1)
+                    return false;
+                }
+            }
+        }
+
+        return smugdeCount==1;
+    }
+
+    public int? FindHorsSymmetry()
+    {
+        for (int r = 0; r < RowsCount - 1; ++r)
+        {
+            if(IsHorsSymmetry(Data, r))
+                return r + 1;
+        }
+        return null;
+    }
+
+    private bool IsHorsSymmetry(string[] Data, int r)
+    {
+        int smugdeCount = 0;
+        int checkHeight = Math.Min(r + 1, RowsCount - r - 1);
+        for (int rr = 0; rr < checkHeight; ++rr)
+            for (int c = 0; c < ColsCount; ++c)
+            {
+                if (Data[r - rr][c] != Data[r + rr + 1][c])
+                {
+                    smugdeCount++;
+                    if (smugdeCount > 1)
+                    return false;
+                }
+            }
+
+        return smugdeCount == 1;
+    }
+}
---
+++ 2023/day14/Program.cs
@@ -0,0 +1,130 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+var map = new Map(Parser.Parse(lines).ToArray(), lines.Length, lines[0].Length);
+Console.WriteLine($"Rocks count: {map.Rocks.Length}");
+int rounds = 1000000000;
+List<long> states = [];
+HashSet<long> loads = [];
+int cycleStart = -1;
+for (int i = 0; i < rounds; ++i)
+{
+    map.MoveNorth();
+    map.MoveWest();
+    map.MoveSouth();
+    map.MoveEast();
+    long state = map.GetStateHash();
+    if (cycleStart < 0)
+    {
+        loads.Add(map.GetLoad());
+        cycleStart = states.IndexOf(state);
+        if (cycleStart >= 0)
+        {
+            int interval = states.Count - cycleStart;
+            int skip = (rounds - i) / interval * interval;
+            Console.WriteLine($"Cycle start {cycleStart}, interval {interval}, will skip {skip}");
+            i += skip;
+        }
+    }
+    states.Add(state);
+}
+
+Console.WriteLine(map.GetLoad());
+loads.Order().Print();
+
+public static class Parser
+{
+    public static IEnumerable<Rock> Parse(string[] lines)
+    {
+        foreach (var (row, col, val) in lines.ForEach())
+        {
+            if (val == 'O')
+                yield return new Rock(row, col, RockType.Rounded);
+            else if (val == '#')
+                yield return new Rock(row, col, RockType.Cube);
+        }
+    }
+}
+
+public class Map(Rock[] rocks, int mapRows, int mapCols)
+{
+    public Rock[] Rocks { get; } = rocks;
+    public int MapRows { get; } = mapRows;
+    public int MapCols { get; } = mapCols;
+
+    public void MoveNorth()
+    {
+        Span<int> colsMaxRow = stackalloc int[MapCols];
+        Array.Sort(Rocks, (a, b) => a.Row - b.Row);
+        foreach (ref Rock rock in Rocks.AsSpan())
+        {
+            if (rock.Type == RockType.Cube)
+                colsMaxRow[rock.Col] = rock.Row + 1;
+            else
+            {
+                rock.Row = colsMaxRow[rock.Col];
+                colsMaxRow[rock.Col]++;
+            }
+        }
+    }
+
+    public void MoveSouth()
+    {
+        Span<int> colsMinRow = stackalloc int[MapCols];
+        Array.Sort(Rocks, (a, b) => b.Row - a.Row);
+        colsMinRow.Fill(MapRows - 1);
+        foreach (ref Rock rock in Rocks.AsSpan())
+        {
+            if (rock.Type == RockType.Cube)
+                colsMinRow[rock.Col] = rock.Row - 1;
+            else
+            {
+                rock.Row = colsMinRow[rock.Col];
+                colsMinRow[rock.Col]--;
+            }
+        }
+    }
+
+    public void MoveWest()
+    {
+        Span<int> rowsMaxRow = stackalloc int[MapRows];
+        Array.Sort(Rocks, (a, b) => a.Col - b.Col);
+
+        foreach (ref Rock rock in Rocks.AsSpan())
+        {
+            if (rock.Type == RockType.Cube)
+                rowsMaxRow[rock.Row] = rock.Col + 1;
+            else
+            {
+                rock.Col = rowsMaxRow[rock.Row];
+                rowsMaxRow[rock.Row]++;
+            }
+        }
+    }
+
+    public void MoveEast()
+    {
+        Span<int> rowsMinCol = stackalloc int[MapRows];
+        Array.Sort(Rocks, (a, b) => b.Col - a.Col);
+        rowsMinCol.Fill(MapCols - 1);
+
+        foreach (ref Rock rock in Rocks.AsSpan())
+        {
+            if (rock.Type == RockType.Cube)
+                rowsMinCol[rock.Row] = rock.Col - 1;
+            else
+            {
+                rock.Col = rowsMinCol[rock.Row];
+                rowsMinCol[rock.Row]--;
+            }
+        }
+
+    }
+    public long GetLoad()
+        => Rocks.Where(r => r.Type == RockType.Rounded).Sum(r => MapRows - r.Row);
+
+    public long GetStateHash()
+        => Rocks.Where(r => r.Type == RockType.Rounded).Sum(r => r.Row * MapRows + r.Col);
+}
+
+public record struct Rock(int Row, int Col, RockType Type);
+
+public enum RockType { Cube, Rounded };
