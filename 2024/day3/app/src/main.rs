24c266e1bb2a4e99d3aacbe08581a036b98b0a27
Alexander Pokislyuk <keyprog@gmail.com>
2024-12-03 10:35:39 -0500

    day2, day3

---
+++ 2024/day3/app/src/main.rs
@@ -0,0 +1,103 @@
+use std::{fmt::format, fs};
+
+const TOKEN_MUL: &str = "mul";
+const TOKEN_OPEN_BR: &str = "(";
+const TOKEN_CLOSE_BR: &str = ")";
+const TOKEN_COMMA: &str = ",";
+
+fn main() {
+    let input_file = fs::canonicalize("../input.txt").unwrap();
+    println!("Reading input from {:?}", input_file);
+
+    let content = fs::read_to_string(&input_file).unwrap();
+    println!("Content:\n{}", content);
+
+    let mut expected_token = TOKEN_MUL;
+    let mut mul1 = 0;
+    let mut mul2 = 0;
+    let mut product: u64 = 0;
+    let mut mul_start = 0;
+
+    let mut i = 0usize;
+    while i < content.len() - expected_token.len() {
+        let token = &content[i..i + expected_token.len()];
+        if token == expected_token {
+            println!("Found {token} at {i}");
+            i = i + token.len();
+            match token {
+                TOKEN_MUL => {
+                    expected_token = TOKEN_OPEN_BR;
+                    mul_start = i - token.len();
+                },
+                TOKEN_OPEN_BR => {
+                    if let (Some(num), len) = parse_num(&content[i..]) {
+                        mul1 = num;
+                        i = i + len;
+                        expected_token = TOKEN_COMMA;
+                    } else {
+                        expected_token = TOKEN_MUL;
+                    }
+                }
+                TOKEN_COMMA => {
+                    if let (Some(num), len) = parse_num(&content[i..]) {
+                        mul2 = num;
+                        i = i + len;
+                        expected_token = TOKEN_CLOSE_BR;
+                    } else {
+                        expected_token = TOKEN_MUL;
+                    }
+                }
+                TOKEN_CLOSE_BR => {
+                    let result = mul1 * mul2;
+                    let str: &str = &content[mul_start..i];
+                    let actual_str = format!("mul({mul1},{mul2})");
+                    product = product + u64::from(result);
+
+                    if str != actual_str {
+                        println!("!!!!!!!!!!!!!!!!");
+                    }
+                    
+                    println!("{actual_str} = {result} from '{str}'");
+                    expected_token = TOKEN_MUL;
+                }
+                _ => panic!("Not implemented"),
+            }
+        } else {
+            if expected_token == TOKEN_MUL {
+                i = i + 1;
+            } else {
+                expected_token = TOKEN_MUL;
+            }
+        }
+    }
+    println!("Result: {product}");
+}
+
+const MAX_NUM_LEN: usize = 3;
+const ZERO: u8 = '0' as u8;
+const TEN: u32 = 10;
+
+fn parse_num(content: &str) -> (Option<u32>, usize) {
+    let mut num: u32 = 0;
+    let mut i:usize  = 0;
+    let max_len = if content.len() > MAX_NUM_LEN {
+        MAX_NUM_LEN
+    } else {
+        content.len()
+    };
+    let bytes = content[0..max_len].as_bytes();
+    if max_len == 0 || !bytes[0].is_ascii_digit() {
+        return (None, 0usize);
+    }
+
+    while i < max_len {
+        if bytes[i].is_ascii_digit() {
+            num = num * 10 + u32::from(bytes[i] - ZERO);
+        } else {
+            break;
+        }
+        i = i + 1;
+    }
+    println!("Found {num}");
+    return (Some(num), i);
+}
