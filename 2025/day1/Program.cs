35194984aa4df8d8e257e5804b6e52ac2dd9d0b5
Alex Pokislyuk <keyprog@gmail.com>
2025-12-01 10:41:55 -0500

    day1 part2

--- 2025/day1/Program.cs
+++ 2025/day1/Program.cs
@@ -2,33 +2,37 @@
 int code = 0;
 
 var instructions = from l in File.ReadLines("/home/alexp/dev/adventofcode/2025/day1/input.txt")
-                    select
-                    (
-                        Dir: l[0],
-                        Dist:  int.Parse(l[1..])
-                    );
-instructions.PrintCollection();
+                   select (Dir: l[0], Dist: int.Parse(l[1..]));
 
-foreach((char dir, int dist) in instructions)
+//instructions.PrintCollection();
+
+foreach (var instruction in instructions)
 {
+    (char dir, int dist) = instruction;
+    if (dist > 100)
+    {
+        code += dist / 100;
+        dist = dist % 100;
+    }
+
     int prevDialNumber = dialNumber;
     dialNumber = dir switch
     {
         'L' => dialNumber - dist,
-        'R' => dialNumber + dist
+        'R' => dialNumber + dist,
+        _ => throw new NotSupportedException()
     };
 
-    (code,dialNumber) = (prevDialNumber, dialNumber) switch
+    (code, dialNumber) = (prevDialNumber, dialNumber) switch
     {
-        (0, >=-100 and <0) => (code + dist / 100, dialNumber + 100),
-        (0, <-100 ) => (code + dist / 100, dialNumber + (100 * (dist/100 + 1))), 
-        (_, <0) => (code + dist / 100 + 1, dialNumber + (100 * (dist/100 + 1))),
-        (_, 0) => (code+1, dialNumber),
-        (_, > 99) => (code + dialNumber / 100, dialNumber % 100),
-        _ => (code, dialNumber)
+        (0, < 0) => (code, dialNumber + 100),
+        (_, < 0) => (code + 1, dialNumber + 100),
+        (_, >=100) => (code + 1, dialNumber - 100),
+        (_, 0) => (code + 1, dialNumber),
+        (_,_) => (code, dialNumber)
     };
 
-    Console.WriteLine($"Code: {code}, dial {dialNumber}");
-       
+    Console.WriteLine($"Instruction: {instruction}, Code: {code}, dial {dialNumber}");
+
 }
 
